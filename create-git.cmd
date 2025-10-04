2c4255a0ba23641d0ef9f08cd73f6ce09198cd51
Jorge Lino Santiesteban Toca <jorge.santiesteban@etecsa.cu>
2025-10-04 00:03:16 -0500

    fix: update remote repository URL in create-git.cmd

--- create-git.cmd
+++ create-git.cmd
@@ -2,5 +2,5 @@ git init
 git add .
 git commit -m "Initial commit"
 git branch -M main
-git remote add origin https://github.com/santiestebantoca/dar-backend.git
+git remote add origin https://github.com/santiestebantoca/admred-backend.git
 git push -u origin main
